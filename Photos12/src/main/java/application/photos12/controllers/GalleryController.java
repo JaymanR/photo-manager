1e6492f5a7ec12f201a6ecff0d2d480b05248423
JaymanR <jayman.code@gmail.com>
2024-03-30 00:05:57 -0400

    implement login method for admin

---
+++ Photos12/src/main/java/application/photos12/controllers/GalleryController.java
@@ -0,0 +1,4 @@
+package application.photos12.controllers;
+
+public class GalleryController {
+}
---
+++ Photos12/src/main/java/application/photos12/controllers/LoginController.java
@@ -0,0 +1,35 @@
+package application.photos12.controllers;
+
+import application.photos12.Photos;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginController {
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private TextField username;
+
+    public void login(ActionEvent e) throws IOException {
+        String name = username.getText();
+        if (name.equals("admin")) {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/application/photos12/admin.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+
+            Photos.changeScene(scene);
+        }
+    }
+}
