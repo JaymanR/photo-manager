351507a2565223afd0bf00f4dc6cbb5cc3e33513
JaymanR <jayman.code@gmail.com>
2024-04-06 02:59:38 -0400

    design album user view

---
+++ Photos12/src/main/java/application/photos12/controllers/AlbumController.java
@@ -0,0 +1,158 @@
+package application.photos12.controllers;
+
+import application.photos12.Photos;
+import application.photos12.model.Album;
+import application.photos12.model.Photo;
+import application.photos12.model.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class AlbumController {
+
+    private static final double PHOTO_THUMBNAIL_SIZE = 100;
+
+    @FXML
+    private BorderPane borderPane;
+
+    private TableView<Album> table;
+
+    private User user;
+    private Scene albumsScene;
+
+    private ObservableList<Album> albumObsList;
+    private Album currentAlbum;
+
+    public void start(Stage stage, User user){
+        this.user = user;
+        initializeAlbums();
+        albumsScene = Photos.getStage().getScene();
+    }
+
+    public void setAlbumsScene() {
+        Photos.getStage().setScene(albumsScene);
+    }
+
+    private void initializeAlbums() {
+        albumObsList = FXCollections.observableArrayList();
+        albumObsList.addAll(user.getAlbums());
+
+        TableColumn<Album, String> nameColumn = new TableColumn<>("Album");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("albumName"));
+
+        TableColumn<Album, Integer> photoCountColumn = new TableColumn<>("Photo Count");
+        photoCountColumn.setCellValueFactory(new PropertyValueFactory<>("numPhotos"));
+
+        table = new TableView<>();
+        table.setItems(albumObsList);
+        table.getColumns().addAll(nameColumn, photoCountColumn);
+
+        borderPane.setCenter(table);
+    }
+
+    public void openAlbum() throws IOException {
+        Album album = table.getSelectionModel().getSelectedItem();
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/application/photos12/view/image-gallery.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+
+        ImageGalleryController imageGalleryController = loader.getController();
+        imageGalleryController.start(album);
+        Photos.getStage().setScene(scene);
+    }
+
+    public void createAlbum() throws IOException {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.initOwner(Photos.window);
+        dialog.setTitle("Create Album");
+        dialog.setHeaderText("Enter AlbumName");
+        Optional<String> result = dialog.showAndWait();
+
+
+        if (result.isPresent() && !user.searchAlb(result.get().toLowerCase())) {
+            if (result.get().isBlank()) {
+                Alert alert = new Alert((Alert.AlertType.INFORMATION));
+                alert.initOwner(Photos.window);
+                alert.setTitle("No Album Name");
+                alert.setHeaderText("Album name not provided.");
+                alert.setContentText("Please enter a name for the Album.");
+                alert.showAndWait();
+                return;
+            }
+            user.mkAlb(result.get().toLowerCase());
+            albumObsList.add(user.getAlbum(result.get()));
+        } else if (result.isPresent() && user.searchAlb(result.get().toLowerCase())) {
+            Alert alert = new Alert((Alert.AlertType.INFORMATION));
+            alert.initOwner(Photos.window);
+            alert.setTitle("Duplicate Album");
+            alert.setHeaderText("Album " + result.get() + " already exists." );
+            alert.setContentText("Please enter another name.");
+            alert.showAndWait();
+        }
+        user.writeUser();
+    }
+
+    public void logout() {
+        Photos.changeScene(Photos.login);
+    }
+
+    public void delete() throws IOException{
+        Album albName = table.getSelectionModel().getSelectedItem();
+        user.delAlb(albName);
+        albumObsList.remove(albName);
+        user.writeUser();
+    }
+
+    public void rename() throws IOException{
+        Album albName = table.getSelectionModel().getSelectedItem();
+
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.initOwner(Photos.window);
+        dialog.setTitle("Rename Album");
+        dialog.setHeaderText("Enter New AlbumName");
+        Optional<String> result = dialog.showAndWait();
+
+        if(result.isPresent() && !user.searchAlb(result.get().toLowerCase())){
+            if(result.get().isBlank()){
+                Alert alert = new Alert((Alert.AlertType.INFORMATION));
+                alert.initOwner(Photos.window);
+                alert.setTitle("No Album Name");
+                alert.setHeaderText("Album name not provided.");
+                alert.setContentText("Please enter a name for the Album.");
+                alert.showAndWait();
+            }
+
+            albName.renameAlbum(result.get().toLowerCase());
+            initializeAlbums();
+
+        }else if(result.isPresent() && user.searchAlb(result.get().toLowerCase())){
+            Alert alert = new Alert((Alert.AlertType.INFORMATION));
+            alert.initOwner(Photos.window);
+            alert.setTitle("Album Name Already Exists");
+            alert.setHeaderText("Album Name is used already.");
+            alert.setContentText("Please enter a new name for Album.");
+            alert.showAndWait();
+
+        }
+        user.writeUser();
+    }
+}
--- Photos12/src/main/java/application/photos12/model/Album.java
+++ Photos12/src/main/java/application/photos12/model/Album.java
@@ -1,22 +1,45 @@
 package application.photos12.model;
 
+import java.io.File;
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Album implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
-    private String albumName;
 
-    public Album(String albumName, User owner) {
+    private String albumName;
+    private ArrayList<Photo> photos;
+    private Integer numPhotos;
+
+    public Album(String albumName) {
         this.albumName = albumName;
+        photos = new ArrayList<>();
+        numPhotos = 0;
     }
 
     public String getAlbumName() {
         return albumName;
     }
 
-    public void rename(String albumName) {
+    public void renameAlbum(String albumName) {
         this.albumName = albumName;
     }
+
+    public void addImage(Photo img) {
+        photos.add(img);
+        numPhotos++;
+    }
+
+    public ArrayList<Photo> getImages(){return photos;}
+    public void removeImage(Photo img) {
+        photos.remove(img);
+        numPhotos--;
+    }
+
+    public Integer getNumPhotos() {
+        return numPhotos;
+    }
+
 }
